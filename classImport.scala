819ad98068aabcb333c8425e4de855c9b8e0428a
Parveen Chauhan <parveen@laptop105.tresata.com>
2024-07-05 17:56:05 +0530

    july 5th

---
+++ classImport.scala
@@ -0,0 +1,7 @@
+import rational._
+object imported extends App{
+    val num1= new Rational(1,2)
+    val num2 = new Rational(3,4)
+    val num3 = num1 +num2
+    println(num3)
+}
---
+++ rational.scala
@@ -0,0 +1,55 @@
+package rational
+
+class Rational(n:Int,d:Int){
+    require(d!=0)
+    private val g = gcd(n.abs, d.abs)
+    val numer: Int =n/g 
+    val denom: Int = d/g
+    
+    def this(n:Int)=this(n,1) //auxiliary constructor used to write an integer as rational number
+    
+    // addition
+    def +(inp : Rational): Rational = {
+        new Rational(numer*inp.denom+inp.numer*denom,denom*inp.denom)
+    }
+    //substraction
+    def -(inp : Rational): Rational = {
+        new Rational(numer*inp.denom-inp.numer*denom,denom*inp.denom)
+    }
+    // multiplicaton
+    def *(inp: Rational): Rational ={
+        new Rational(numer*inp.numer, denom*inp.denom)
+    }
+    //division
+    def /(inp:Rational):Rational={
+        new Rational(numer*inp.denom,denom*inp.numer)
+    }
+    
+    //to calculate the gcd of two numbers.
+    private def gcd(num1:Int , num2:Int):Int={
+        if(num2==0) num1
+        else gcd(num2,num1%num2)
+    }
+
+    override def toString= s"$numer/$denom"
+
+    //now begin method overloading...
+    //overloaded addition
+    def +(inp:Int): Rational= new Rational(numer+inp*denom,denom)
+    //overloaded subsraction
+    def -(inp:Int):Rational = new Rational(numer-inp*denom,denom)
+    //overloaded multiplication
+    def *(inp:Int ):Rational= new Rational(numer*inp,denom)
+    //overloaded division
+    def /(inp:Int): Rational= new Rational(numer,denom*inp)
+}
+ 
+object Rational extends App{
+    val a = new Rational(3,2)
+    val b = new Rational(1,2)
+    val c = a+b //or we can use a.+b (using it as an expressin instead of operator)
+    val d= a*b
+    println("after addition "  +c  ) //  gives 2/1
+    
+    println("after multiplication " +d) // gives 3/4
+}
